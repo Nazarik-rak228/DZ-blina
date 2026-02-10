474cd019382813748561bed85b2f409d21a52878
Nazar <nazarbragin228@gmail.com>
2026-02-10 15:38:29 +0300

    Добавил asppe

--- ASP.NET/BK/BK/BK/obj/Release/net8.0/BK.AssemblyInfo.cs
+++ ASP.NET/BK/BK/BK/obj/Release/net8.0/BK.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BK")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b0743959fe0f1d47cdfdbb74d3e4bc37391c80b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BK")]
--- ASP.NET/BK/BK/BK/obj/Release/net8.0/BK.RazorAssemblyInfo.cs
+++ ASP.NET/BK/BK/BK/obj/Release/net8.0/BK.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("BK")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b0743959fe0f1d47cdfdbb74d3e4bc37391c80b")]
-[assembly: System.Reflection.AssemblyProductAttribute("BK")]
-[assembly: System.Reflection.AssemblyTitleAttribute("BK")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
